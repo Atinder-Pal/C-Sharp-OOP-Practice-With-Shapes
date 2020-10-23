01a6b3cdce4a7ead4106a9cf100e94d6fde03d6d
Atinder-Pal <atty.pal46@gmail.com>
2020-10-23 17:36:14 -0600

    Create Traingle Class;

--- Shapes_Practice/Shapes_Practice/Shapes/Triangle.cs
+++ Shapes_Practice/Shapes_Practice/Shapes/Triangle.cs
@@ -4,8 +4,30 @@ using System.Text;
 
 namespace Shapes_Practice.Shapes
 {
-    class Triangle
+    class Triangle: Shape
     {
+        public Double Base { get; set; }
+        public Double Height { get; set; }
+        public override double Perimeter
+        {
+            get
+            {
+                return Base * 3;
+            }
+        }
 
+        public override double Area
+        {
+            get
+            {
+                return (Base * Height) / 2;
+            }
+        }
+
+        public Triangle( String colour, double triangleBase, double height ):base(colour)
+        {
+            Base = triangleBase;
+            Height = height;
+        }
     }
 }
